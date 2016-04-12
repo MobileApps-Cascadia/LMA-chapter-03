5a95b78b67ee36a73bc4dcd03608c843f6fb0a5a
Liz Fallin <liz.fallin@gmail.com>
2016-04-12 12:06:49 -0700

    Turn in April 12

--- app/src/main/java/com/example/helloworld/HelloWorldActivity.java
+++ app/src/main/java/com/example/helloworld/HelloWorldActivity.java
@@ -1,5 +1,7 @@
 package com.example.helloworld;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -9,38 +11,95 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class HelloWorldActivity extends Activity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_world);
-        
+
         initDisplayButton();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action bar if it is present.Li
+        // "R." refers to resource class, which is a class derived from xml
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
-    private void initDisplayButton() {
-    	Button displayButton = (Button) findViewById(R.id.buttonDisplay);
-    	displayButton.setOnClickListener(new OnClickListener () {
 
-			@Override
-			public void onClick(View arg0) {
-				EditText editName = (EditText) findViewById(R.id.editTextName);
-				TextView textDisplay = (TextView) findViewById(R.id.textViewDisplay);
-				String nameToDisplay = editName.getText().toString();
-				textDisplay.setText("Hello " + nameToDisplay);
-				
-			}
-    		
-    	});
+    private void initDisplayButton() {
+        Button displayButton = (Button) findViewById(R.id.buttonDisplay);
+        displayButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                EditText editName = (EditText) findViewById(R.id.editTextName);
+                TextView textDisplay = (TextView) findViewById(R.id.textViewDisplay);
+                String nameToDisplay = editName.getText().toString();
+
+                Intent myIntent = new Intent(getApplicationContext(), SecondActivity.class);
+                myIntent.putExtra("UserName", nameToDisplay);
+                startActivity(myIntent);
+                //textDisplay.setText("Hello " + nameToDisplay);
+
+            }
+
+        });
     }
-    
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "HelloWorld Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.helloworld/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "HelloWorld Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.helloworld/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
